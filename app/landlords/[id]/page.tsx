c77705f558fc6acab41e653590628807c0ab29a8
Priyatham10 <kunarapupriyatham@gmail.com>
2025-01-29 15:53:17 +0530

    Added Nababar, Categories, Poperty details pages

---
+++ app/landlords/[id]/page.tsx
@@ -0,0 +1,9 @@
+const LandlordDetailPage = () => {
+    return (
+        <main className="max-w-[1500px] mx-auto px-6 pb-6">
+            landlord
+        </main>
+    )
+}
+
+export default LandlordDetailPage;
